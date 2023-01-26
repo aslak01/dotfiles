aad188119b034c6a9f4375057d31ea555dd9dac6
aslak <aslakrb@gmail.com>
2023-01-27 00:06:36 +0100

    fill finicky

---
+++ .finicky.js
@@ -0,0 +1,10 @@
+module.exports = {
+	defaultBrowser: 'Safari',
+	handlers: [
+		{
+			match: ['everyday.no/*', '*.everyday.no/*', '*.monday.com/*', 'monday.com/*', '*.serif.com/*', 'serif.com/*', '*.basecamp.com/*', 'basecamp.com/*', '*.cloudflare.com/*', 'cloudflare.com/*', '*.google.com/*', 'google.com/*', '*.atlassian.com/*', 'atlassian.com/*', '*.figma.com/*', 'figma.com/*', '*.folq.com/*', 'folq.com/*', 'notion.so', '*.notion.so/*', 'teams.microsoft.com/*'],
+			browser: '/Applications/Google Chrome.app',
+		},
+	],
+	options: { logRequests: true },
+};
