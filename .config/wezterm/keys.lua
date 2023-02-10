ee103a10aff8837065a76a02920f49cf7741d093
kbjh,jh <hkjhkj>
2023-02-10 12:44:00 +0100

    cleaning up
    cl

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -229,3 +229,79 @@ M.key_tables = {
 }
 
 return M
+
+
+  -- from disabled config: 
+  -- keys = {
+  --   {
+  --     mods = "ALT",
+  --     key = [[\]],
+  --     action = wezterm.action({
+  --       SplitHorizontal = { domain = "CurrentPaneDomain" },
+  --     }),
+  --   },
+  --   {
+  --     mods = "ALT|SHIFT",
+  --     key = [[|]],
+  --     action = wezterm.action.SplitPane({
+  --       top_level = true,
+  --       direction = "Right",
+  --       size = { Percent = 50 },
+  --     }),
+  --   },
+  --   {
+  --     mods = "ALT",
+  --     key = [[-]],
+  --     action = wezterm.action({
+  --       SplitVertical = { domain = "CurrentPaneDomain" },
+  --     }),
+  --   },
+  --   {
+  --     mods = "ALT|SHIFT",
+  --     key = [[_]],
+  --     action = wezterm.action.SplitPane({
+  --       top_level = true,
+  --       direction = "Down",
+  --       size = { Percent = 50 },
+  --     }),
+  --   },
+  --   {
+  --     key = "n",
+  --     mods = "ALT",
+  --     action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
+  --   },
+  --   {
+  --     key = "Q",
+  --     mods = "ALT",
+  --     action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
+  --   },
+  --   { key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+  --   { key = "z", mods = "ALT", action = wezterm.action.TogglePaneZoomState },
+  --   { key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
+  --   { key = "h", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+  --   { key = "j", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+  --   { key = "k", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+  --   { key = "l", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+  --   { key = "h", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
+  --   { key = "j", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
+  --   { key = "k", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
+  --   { key = "l", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
+  --   { key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+  --   { key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
+  --   { key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
+  --   { key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
+  --   { key = "v", mods = "ALT", action = wezterm.action.ActivateCopyMode },
+  --   { key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+  --   { key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+  --   { key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+  --   { key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+  --   { key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
+  --   { key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
+  --   { key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
+  --   { key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
+  --   { key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
+  --   { key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
+  --   { key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
+  --   { key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
+  --   { key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
+  -- },
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,94 +1,42 @@
+-- wezterm config inspired by https://github.com/thanhvule0310/dotfiles
 local wezterm = require("wezterm")
 local keymaps = require("keys")
--- local colors = require "colors"
--- local is_dark = wezterm.gui.get_appearance():find("Dark")
-local is_dark = true
--- local function is_vi_process(pane)
---   return pane:get_foreground_process_name():find("n?vim") ~= nil
--- end
+local process_icons = require("process_icons")
 
--- local function conditional_activate_pane(window, pane, pane_direction, vim_direction)
---   if is_vi_process(pane) then
---     window:perform_action(wezterm.action.SendKey({ key = vim_direction, mods = "ALT" }), pane)
---   else
---     window:perform_action(wezterm.action.ActivatePaneDirection(pane_direction), pane)
---   end
--- end
+local function is_vi_process(pane)
+  return pane:get_foreground_process_name():find("n?vim") ~= nil
+end
 
--- wezterm.on("ActivatePaneDirection-right", function(window, pane)
---   conditional_activate_pane(window, pane, "Right", "l")
--- end)
--- wezterm.on("ActivatePaneDirection-left", function(window, pane)
---   conditional_activate_pane(window, pane, "Left", "h")
--- end)
--- wezterm.on("ActivatePaneDirection-up", function(window, pane)
---   conditional_activate_pane(window, pane, "Up", "k")
--- end)
--- wezterm.on("ActivatePaneDirection-down", function(window, pane)
---   conditional_activate_pane(window, pane, "Down", "j")
--- end)
+local function conditional_activate_pane(window, pane, pane_direction, vim_direction)
+  if is_vi_process(pane) then
+    window:perform_action(wezterm.action.SendKey({ key = vim_direction, mods = "ALT" }), pane)
+  else
+    window:perform_action(wezterm.action.ActivatePaneDirection(pane_direction), pane)
+  end
+end
+
+wezterm.on("ActivatePaneDirection-right", function(window, pane)
+  conditional_activate_pane(window, pane, "Right", "l")
+end)
+wezterm.on("ActivatePaneDirection-left", function(window, pane)
+  conditional_activate_pane(window, pane, "Left", "h")
+end)
+wezterm.on("ActivatePaneDirection-up", function(window, pane)
+  conditional_activate_pane(window, pane, "Up", "k")
+end)
+wezterm.on("ActivatePaneDirection-down", function(window, pane)
+  conditional_activate_pane(window, pane, "Down", "j")
+end)
 
 local function get_process(tab)
-  local process_icons = {
-    ["docker"] = {
-      { Text = wezterm.nerdfonts.linux_docker },
-    },
-    ["docker-compose"] = {
-      { Text = wezterm.nerdfonts.linux_docker },
-    },
-    ["nvim"] = {
-      { Text = wezterm.nerdfonts.custom_vim },
-    },
-    ["vim"] = {
-      { Text = wezterm.nerdfonts.dev_vim },
-    },
-    ["node"] = {
-      { Text = wezterm.nerdfonts.mdi_hexagon },
-    },
-    ["zsh"] = {
-      { Text = wezterm.nerdfonts.dev_terminal },
-    },
-    ["bash"] = {
-      { Text = wezterm.nerdfonts.cod_terminal_bash },
-    },
-    ["paru"] = {
-      { Text = wezterm.nerdfonts.linux_archlinux },
-    },
-    ["htop"] = {
-      { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
-    },
-    ["cargo"] = {
-      { Text = wezterm.nerdfonts.dev_rust },
-    },
-    ["go"] = {
-      { Text = wezterm.nerdfonts.mdi_language_go },
-    },
-    ["lazydocker"] = {
-      { Text = wezterm.nerdfonts.linux_docker },
-    },
-    ["git"] = {
-      { Text = wezterm.nerdfonts.dev_git },
-    },
-    ["lazygit"] = {
-      { Text = wezterm.nerdfonts.dev_git },
-    },
-    ["lua"] = {
-      { Text = wezterm.nerdfonts.seti_lua },
-    },
-    ["wget"] = {
-      { Text = wezterm.nerdfonts.mdi_arrow_down_box },
-    },
-    ["curl"] = {
-      { Text = wezterm.nerdfonts.mdi_flattr },
-    },
-    ["gh"] = {
-      { Text = wezterm.nerdfonts.dev_github_badge },
-    },
-  }
   local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
 
+  if process_name == "" then
+    process_name = "zsh"
+  end
+
   return wezterm.format(
-    process_icons[process_name]
+    process_icons.icons[process_name]
     or { { Text = string.format("[%s]", process_name) } }
   )
 end
@@ -97,25 +45,25 @@ local function get_current_working_dir(tab)
   local current_dir = tab.active_pane.current_working_dir
   local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
 
-  return current_dir == HOME_DIR and "  ~"
-      or string.format("  %s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
+  return current_dir == HOME_DIR and "~"
+      or string.format("%s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
 end
 
 wezterm.on("format-tab-title", function(tab)
   return wezterm.format({
     { Attribute = { Intensity = "Half" } },
-    { Text = string.format(" %s  ", tab.tab_index + 1) },
+    { Text = string.format(" %s ", tab.tab_index + 1) },
     "ResetAttributes",
     { Text = get_process(tab) },
     { Text = " " },
     { Text = get_current_working_dir(tab) },
-    { Text = "  ▕" },
+    { Text = "▕" },
   })
 end)
 
 wezterm.on("update-right-status", function(window)
   window:set_right_status(wezterm.format({
-    { Attribute = { Intensity = "Bold" } },
+    { Attribute = { Intensity = "Normal" } },
     { Text = wezterm.strftime(" %A, %d %B %Y %-H:%M ") },
   }))
 end)
@@ -163,79 +111,6 @@ return {
   use_dead_keys = true,
   debug_key_events = false,
   front_end = "WebGpu",
-  -- keys = {
-  --   {
-  --     mods = "ALT",
-  --     key = [[\]],
-  --     action = wezterm.action({
-  --       SplitHorizontal = { domain = "CurrentPaneDomain" },
-  --     }),
-  --   },
-  --   {
-  --     mods = "ALT|SHIFT",
-  --     key = [[|]],
-  --     action = wezterm.action.SplitPane({
-  --       top_level = true,
-  --       direction = "Right",
-  --       size = { Percent = 50 },
-  --     }),
-  --   },
-  --   {
-  --     mods = "ALT",
-  --     key = [[-]],
-  --     action = wezterm.action({
-  --       SplitVertical = { domain = "CurrentPaneDomain" },
-  --     }),
-  --   },
-  --   {
-  --     mods = "ALT|SHIFT",
-  --     key = [[_]],
-  --     action = wezterm.action.SplitPane({
-  --       top_level = true,
-  --       direction = "Down",
-  --       size = { Percent = 50 },
-  --     }),
-  --   },
-  --   {
-  --     key = "n",
-  --     mods = "ALT",
-  --     action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
-  --   },
-  --   {
-  --     key = "Q",
-  --     mods = "ALT",
-  --     action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
-  --   },
-  --   { key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-  --   { key = "z", mods = "ALT", action = wezterm.action.TogglePaneZoomState },
-  --   { key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
-  --   { key = "h", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
-  --   { key = "j", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
-  --   { key = "k", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
-  --   { key = "l", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
-  --   { key = "h", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
-  --   { key = "j", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
-  --   { key = "k", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
-  --   { key = "l", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
-  --   { key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-  --   { key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
-  --   { key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
-  --   { key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
-  --   { key = "v", mods = "ALT", action = wezterm.action.ActivateCopyMode },
-  --   { key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-  --   { key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-  --   { key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-  --   { key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-  --   { key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
-  --   { key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
-  --   { key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
-  --   { key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
-  --   { key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
-  --   { key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
-  --   { key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
-  --   { key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
-  --   { key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
-  -- },
   hyperlink_rules = {
     {
       regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
