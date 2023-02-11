45351362e3cfa393adc0fdcdb5fdce634ddfbf50
aslak <aslakrb@gmail.com>
2023-02-11 12:12:21 +0100

    amethyst config & lazyvim nvim

---
+++ .config/old-nvim/lua/config/keymaps.lua
@@ -0,0 +1,77 @@
+-- @keymaps
+local keyset = vim.keymap.set
+
+keyset("n", "<down>", "}")
+keyset("n", "<up>", "{")
+-- keyset("n", "<leader>q", ":bd<cr>")
+keyset("n", "<leader>w", ":w<cr>")
+-- keyset("n", "<C-space>", "/")
+keyset("n", "<leader>d", vim.diagnostic.open_float)
+keyset("n", "<leader>hh", vim.diagnostic.goto_prev)
+keyset("n", "<leader>ll", vim.diagnostic.goto_next)
+keyset("n", "<leader>fn", ":Telescope file_browser<CR>")
+
+-- ripgrep
+keyset("n", "<leader>rg", ":Rg<space>")
+
+-- commentary
+-- keyset("n", "<C-/>", ":Commentary<cr>")
+-- keyset("v", "<C-/>", ":Commentary<cr>")
+
+-- vim-test
+keyset("n", "<leader>tn", ":TestNearest<cr>", { silent = true })
+keyset("n", "<leader>tf", ":TestFile<cr>", { silent = true })
+keyset("n", "<leader>ts", ":TestSuite<cr>", { silent = true })
+keyset("n", "<leader>tl", ":TestLast<cr>", { silent = true })
+keyset("n", "<leader>tv", ":TestVisit<cr>", { silent = true })
+
+-- packer
+keyset("n", "<leader>pi", ":PackerInstall<cr>")
+keyset("n", "<leader>pu", ":PackerUpdate<cr>")
+keyset("n", "<leader>pc", ":PackerClean<cr>")
+
+-- fugitive
+keyset("n", "<leader>gd", ":Gdiff<cr>")
+
+-- telescope
+keyset("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[F]ind [F]iles" })
+keyset("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "[F]ind [G]rep" })
+keyset("n", "<leader>fs", require("telescope.builtin").git_status, { desc = "[F]ind [G]it status" })
+keyset("n", "<leader>fh", require("telescope.builtin").oldfiles, { desc = "[F]ind [H]istory" })
+keyset("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "[F]ind [B]uffers" })
+keyset("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "[F]ind [W]ord" })
+keyset("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "[F]ind [D]iagnostics" })
+keyset("n", "<leader>fc", require("telescope.builtin").commands, { desc = "[F]ind [C]ommands" })
+keyset("n", "<leader>km", require("telescope.builtin").keymaps, { desc = "[K]ey[m]aps" })
+
+-- harpoon
+keyset("n", "<leader>m", require("harpoon.mark").add_file)
+keyset("n", "<leader>fm", ":Telescope harpoon marks<cr>")
+
+-- lsp - set lsp mappings in callback
+function SetCustomLspMappings(bufnr)
+	keyset("n", "gd", vim.lsp.buf.definition, { desc = "[G]oto [D]efinition" })
+	keyset("n", "gI", vim.lsp.buf.implementation, { desc = "[G]oto [I]mplementation" })
+	keyset("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Type [D]efinition" })
+	keyset("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+	keyset("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+	keyset("n", "<leader>f", vim.lsp.buf.format, { desc = "[F]ormat current buffer" })
+
+	keyset("n", "gr", require("telescope.builtin").lsp_references, { desc = "[G]oto [R]eferences" })
+	keyset("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
+	keyset(
+		"n",
+		"<leader>ws",
+		require("telescope.builtin").lsp_dynamic_workspace_symbols,
+		{ desc = "[W]orkspace [S]ymbols" }
+	)
+
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		if vim.lsp.buf.format then
+			vim.lsp.buf.format()
+		elseif vim.lsp.buf.formatting then
+			vim.lsp.buf.formatting()
+		end
+	end, { desc = "Format current buffer with LSP" })
+end
--- .config/old-nvim/lua/config/lsp.lua
+++ .config/old-nvim/lua/config/lsp.lua
@@ -1,6 +1,6 @@
 -- Lsp
 -- turn on lsp status information
-require("fidget").setup()
+-- require("fidget").setup()
 
 -- Mason
 require("mason").setup()
@@ -9,38 +9,38 @@ lsp.preset("recommended")
 
 -- disable defaults mappings
 lsp.set_preferences({
-	set_lsp_keymaps = false,
+  set_lsp_keymaps = false,
 })
 
 -- enable deno detection
 lsp.configure("tsserver", {
-	single_file_support = false,
-	root_dir = require("lspconfig.util").root_pattern("package.json"),
+  single_file_support = false,
+  root_dir = require("lspconfig.util").root_pattern("package.json"),
 })
 lsp.configure("denols", {
-	root_dir = require("lspconfig.util").root_pattern("deno.json"),
+  root_dir = require("lspconfig.util").root_pattern("deno.json"),
 })
 
 -- lsp servers based on: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 lsp.ensure_installed({
-	-- web
-	"html",
-	"tsserver",
-	"cssls",
-	"stylelint_lsp",
-	"eslint",
-	"denols",
-	"bashls",
-	"sumneko_lua",
+  -- web
+  "html",
+  "tsserver",
+  "cssls",
+  "stylelint_lsp",
+  "eslint",
+  "denols",
+  "bashls",
+  "sumneko_lua",
 })
 
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<C-y>"] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 -- disable completion with tab
@@ -49,11 +49,11 @@ cmp_mappings["<Tab>"] = nil
 cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings,
+  mapping = cmp_mappings,
 })
 
 lsp.on_attach(function(_, bufnr)
-	SetCustomLspMappings(bufnr)
+  SetCustomLspMappings(bufnr)
 end)
 
 lsp.nvim_workspace()
