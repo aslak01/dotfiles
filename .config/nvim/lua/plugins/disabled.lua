ed48ab754f7a88e9e3cd0a5f5a148dd50e10d50d
aslak <aslakrb@gmail.com>
2023-02-11 16:40:57 +0100

    upd nvim conf

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  -- disable noice
+  { "folke/noice.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+}
