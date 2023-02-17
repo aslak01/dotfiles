855dd721942878cfc72470c92602375bf64d4b3e
aslak <aslakrb@gmail.com>
2023-02-17 16:33:33 +0100

    upd keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+-- remove keybinds I don't like:
+local del = vim.keymap.del
+del("n", "<leader>l")
+
+-- add keybinds I want, or modify existing to work for me
+local set = vim.keymap.set
+
+set("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true, desc = "Resume" })
