4033e337ce8468f5739fe6c6e548ce8ddc9d47f1
aslak <aslakrb@gmail.com>
2023-03-06 21:50:21 +0100

    support deno

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -4,4 +4,8 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  -- not honoring root patterns, messing up deno
+  -- https://github.com/LazyVim/LazyVim/issues/295
+  -- https://github.com/folke/neoconf.nvim/issues/14
+  { "folke/neoconf.nvim", enabled = false },
 }
