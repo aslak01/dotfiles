61be093074e4c7f5f5d724281ab27cf9fc69bd03
as <aslak.bakkeland@medialaben.no>
2023-03-13 14:18:47 +0100

    upd finicky

--- .finicky.js
+++ .finicky.js
@@ -2,7 +2,7 @@ module.exports = {
 	defaultBrowser: 'Safari',
 	handlers: [
 		{
-			match: ['everyday.no/*', '*.everyday.no/*', '*.monday.com/*', 'monday.com/*', '*.serif.com/*', 'serif.com/*', '*.basecamp.com/*', 'basecamp.com/*', '*.cloudflare.com/*', 'cloudflare.com/*', '*.google.com/*', 'google.com/*', '*.atlassian.com/*', 'atlassian.com/*', '*.figma.com/*', 'figma.com/*', '*.folq.com/*', 'folq.com/*', 'notion.so', '*.notion.so/*', 'teams.microsoft.com/*'],
+			match: ['*.labdevs.com/*', '*.trello.com/*', '*.monday.com/*', 'monday.com/*', '*.serif.com/*', 'serif.com/*', '*.basecamp.com/*', 'basecamp.com/*', '*.cloudflare.com/*', 'cloudflare.com/*', '*.google.com/*', 'google.com/*', '*.atlassian.com/*', 'atlassian.com/*', '*.figma.com/*', 'figma.com/*', '*.folq.com/*', 'folq.com/*', 'notion.so', '*.notion.so/*', 'teams.microsoft.com/*'],
 			browser: '/Applications/Google Chrome.app',
 		},
 	],
