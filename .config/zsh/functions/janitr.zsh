a2075d5d77d2f1ebe7a55f11ff6503a6bc58e716
as <aslak.bakkeland@medialaben.no>
2023-11-17 23:02:47 +0100

    add janitr prod ban

--- .config/zsh/functions/janitr.zsh
+++ .config/zsh/functions/janitr.zsh
@@ -2,16 +2,11 @@
 
 function janitrStageBanChannel() {
     REQ="$1"
-
     BASE_URL="$JANITR_STAGE_URL"
     AUTH="'Authorization: Basic' "$JANITR_STAGE_TOKEN""
-
     URL="$BASE_URL"ban?keys[]="$REQ"
-
     # echo "--location -g $URL --header "$AUTH""
-
     curl --location -g $URL --header "$AUTH"
-
 }
 
 function janitrStageBanUrl() {
@@ -19,8 +14,15 @@ function janitrStageBanUrl() {
     BASE_URL="$JANITR_STAGE_URL"
     AUTH="'Authorization: Basic' "$JANITR_STAGE_TOKEN""
     URL="$BASE_URL"ban/$REQ
-
     # echo "--location -g $URL --header "$AUTH""
+    curl --location -g $URL --header "$AUTH"
+}
 
+function janitrProdBanChannel() {
+    REQ="$1"
+    BASE_URL="$JANITR_URL"
+    AUTH="'Authorization: Basic' "$JANITR_TOKEN""
+    URL="$BASE_URL"ban?keys[]="$REQ"
+    # echo "--location -g $URL --header "$AUTH""
     curl --location -g $URL --header "$AUTH"
 }
