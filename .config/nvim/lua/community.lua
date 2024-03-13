f51ee9f3064b402b35f5b201c834a3ed6d071d0f
as <aslak.bakkeland@medialaben.no>
2024-03-14 00:27:10 +0100

    v4upd

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,39 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  -- TODO: Remove branch v4 on release
+  { "AstroNvim/astrocommunity", branch = "v4" },
+
+  -- lang specific lsp etc
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.gleam" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+
+  -- general lsp
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
+  { import = "astrocommunity.project.nvim-spectre" },
+
+  -- orchestration
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+
+  -- github
+  { import = "astrocommunity.git.octo-nvim" },
+  { import = "astrocommunity.git.openingh-nvim" },
+
+  -- color
+  { import = "astrocommunity.color.ccc-nvim" },
+
+  -- editor tweaks
+  { import = "astrocommunity.editing-support.mini-splitjoin" },
+}
