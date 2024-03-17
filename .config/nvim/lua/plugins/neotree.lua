e7cd610aab31f6face915fd6b5167ee032f6a762
as <aslak.bakkeland@medialaben.no>
2024-03-18 00:34:09 +0100

    configs

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      mappings = {
+        ["o"] = "open"
+      }
+    },
     filesystem = {
       filtered_items = {
         -- when true, they will just be displayed differently than normal items
