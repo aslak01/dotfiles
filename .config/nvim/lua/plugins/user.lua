ec984818f62bea2dd6add01164b1a0a34de0dd06
aslak <aslakrb@gmail.com>
2024-05-21 01:20:41 +0200

    cleanup

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -1,10 +1,6 @@
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
-
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
+  -- auto switching zenbones theme
   { "rktjmp/lush.nvim" },
   { "mcchrish/zenbones.nvim" },
   {
@@ -18,6 +14,7 @@ return {
     end,
     init = function() require("auto-dark-mode").init() end,
   },
+
   {
     "tjdevries/ocaml.nvim",
     build = ":lua require('ocaml').update()",
@@ -31,71 +28,71 @@ return {
   -- == Examples of Overriding Plugins ==
 
   -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        "",
-      }
-      return opts
-    end,
-  },
+  -- {
+  --   "goolord/alpha-nvim",
+  --   opts = function(_, opts)
+  --     -- customize the dashboard header
+  --     opts.section.header.val = {
+  --       "",
+  --     }
+  --     return opts
+  --   end,
+  -- },
 
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   config = function(plugin, opts)
+  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+  --     -- add more custom luasnip configuration such as filetype extend or custom snippets 
+  --     local luasnip = require "luasnip"
+  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
+  --   end,
+  -- },
 
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
-  {
-    "echasnovski/mini.surround",
-    keys = {
-      { "sa", desc = "Add surrounding", mode = { "n", "v" } },
-      { "sd", desc = "Delete surrounding" },
-      { "sf", desc = "Find right surrounding" },
-      { "sF", desc = "Find left surrounding" },
-      { "sh", desc = "Highlight surrounding" },
-      { "sr", desc = "Replace surrounding" },
-      { "sn", desc = "Update `MiniSurround.config.n_lines`" },
-    },
-    opts = { n_lines = 200 },
-  },
+  -- {
+  --   "windwp/nvim-autopairs",
+  --   config = function(plugin, opts)
+  --     require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+  --     -- add more custom autopairs configuration such as custom rules
+  --     local npairs = require "nvim-autopairs"
+  --     local Rule = require "nvim-autopairs.rule"
+  --     local cond = require "nvim-autopairs.conds"
+  --     npairs.add_rules(
+  --       {
+  --         Rule("$", "$", { "tex", "latex" })
+  --           -- don't add a pair if the next character is %
+  --           :with_pair(cond.not_after_regex "%%")
+  --           -- don't add a pair if  the previous character is xxx
+  --           :with_pair(
+  --             cond.not_before_regex("xxx", 3)
+  --           )
+  --           -- don't move right when repeat character
+  --           :with_move(cond.none())
+  --           -- don't delete if the next character is xx
+  --           :with_del(cond.not_after_regex "xx")
+  --           -- disable adding a newline when you press <cr>
+  --           :with_cr(cond.none()),
+  --       },
+  --       -- disable for .vim files, but it work for another filetypes
+  --       Rule("a", "a", "-vim")
+  --     )
+  --   end,
+  -- },
+  -- {
+  --   "echasnovski/mini.surround",
+  --   keys = {
+  --     { "sa", desc = "Add surrounding", mode = { "n", "v" } },
+  --     { "sd", desc = "Delete surrounding" },
+  --     { "sf", desc = "Find right surrounding" },
+  --     { "sF", desc = "Find left surrounding" },
+  --     { "sh", desc = "Highlight surrounding" },
+  --     { "sr", desc = "Replace surrounding" },
+  --     { "sn", desc = "Update `MiniSurround.config.n_lines`" },
+  --   },
+  --   opts = { n_lines = 200 },
+  -- },
 }
