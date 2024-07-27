feb55a9f7a7babd415da5d103ade0514456d16b6
aslak <aslakrb@gmail.com>
2024-07-28 01:11:01 +0200

    replace neotree with oil

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,37 @@
+return {
+  "stevearc/oil.nvim",
+  init = function() -- start oil on startup lazily if necessary
+    if vim.fn.argc() == 1 then
+      local arg = vim.fn.argv(0)
+      ---@cast arg string
+      local stat = (vim.uv or vim.loop).fs_stat(arg)
+      local adapter = string.match(arg, "^([%l-]*)://")
+      if (stat and stat.type == "directory") or adapter == "oil-ssh" then require "oil" end
+    end
+  end,
+  specs = {
+    { "nvim-neo-tree/neo-tree.nvim", optional = true, opts = { filesystem = { hijack_netrw_behavior = "disabled" } } },
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          neotree_start = false,
+          oil_start = {
+            {
+              event = "BufNew",
+              desc = "start oil when editing a directory",
+              callback = function()
+                if package.loaded["oil"] then
+                  vim.api.nvim_del_augroup_by_name "oil_start"
+                elseif vim.fn.isdirectory(vim.fn.expand "<afile>") == 1 then
+                  require "oil"
+                  vim.api.nvim_del_augroup_by_name "oil_start"
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+}
