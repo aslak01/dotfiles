ace850a893a65432c8e6b0a072260ecdc6e52140
aslak <aslakrb@gmail.com>
2024-08-05 00:09:49 +0200

    move out scripts

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 # Homebrewed scripts
-source "$HOME/.config/zsh/functions/compressions.zsh"
+# source "$HOME/.config/zsh/functions/compressions.zsh"
 source "$HOME/.config/zsh/functions/janitr.zsh"
