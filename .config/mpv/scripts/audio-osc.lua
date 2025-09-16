35eab601d85827eb10f41e852613d267b41d5cae
Aslak <hei@asla.cc>
2025-09-16 23:18:12 +0200

    add neat mpv config

--- .config/mpv/scripts/audio-osc.lua
+++ .config/mpv/scripts/audio-osc.lua
@@ -2,8 +2,9 @@
 -- source: https://github.com/mpv-player/mpv/issues/3500#issuecomment-305646994
 
 mp.register_event("file-loaded", function()
-    local hasvid = mp.get_property_osd("video") ~= "no"
-    mp.commandv("script-message", "osc-visibility", (hasvid and "auto" or "always"), "no-osd")
-    -- remove the next line if you don't want to affect the osd-bar config
-    mp.commandv("set", "options/osd-bar", (hasvid and "yes" or "no"))
+	local hasvid = mp.get_property_osd("video") ~= "no"
+	mp.commandv("script-message", "osc-visibility", (hasvid and "auto" or "always"), "no-osd")
+	-- remove the next line if you don't want to affect the osd-bar config
+	mp.commandv("set", "options/osd-bar", (hasvid and "yes" or "no"))
 end)
+
