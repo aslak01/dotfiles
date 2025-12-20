50acd92c138a85510d00be97d622157763002ff9
Aslak <hei@asla.cc>
2025-12-20 12:42:59 +0100

    scrollback and easy align

---
+++ .config/nvim/lua/plugins/easy-align.lua
@@ -0,0 +1,8 @@
+return {
+  "junegunn/vim-easy-align",
+  event = "User AstroFile",
+  keys = {
+    { "ga", "<Plug>(EasyAlign)", mode = "n", desc = "EasyAlign" },
+    { "ga", "<Plug>(EasyAlign)", mode = "x", desc = "EasyAlign" },
+  },
+}
